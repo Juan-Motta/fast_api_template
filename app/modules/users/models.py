1be64058fad13567846fdd6a3fcced7173919361
Juan Andres Lopez Motta <juan@192.168.0.3>
2023-04-15 00:28:11 -0500

    added password hash to user model

--- app/modules/users/models.py
+++ app/modules/users/models.py
@@ -1,8 +1,12 @@
+import hashlib
+import binascii
+
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 from app.database.base import Base
 from app.database.mixins import TimestampMixin
+from app.config.settings import settings
 
 
 class User(Base, TimestampMixin):
@@ -16,6 +20,30 @@ class User(Base, TimestampMixin):
     
     profile_id = Column(Integer, ForeignKey("users_profiles.id"))
     user_profiles = relationship("UserProfile", back_populates="users")
+    
+    @property
+    def password(self) -> None:
+        raise AttributeError('password is not a readable attribute')
+    
+    def set_password(self, password: str) -> None:
+        dk = hashlib.pbkdf2_hmac(
+            "sha256", 
+            password.encode("utf-8"), 
+            settings.SECRET_KEY.encode("utf-8"), 
+            100000
+        )
+        hashed_password = binascii.hexlify(dk).decode("utf-8")
+        self.password = f"pbkdf2_sha256$100000${hashed_password}"
+
+    def check_password(self, password: str) -> bool:
+        dk = hashlib.pbkdf2_hmac(
+            "sha256", 
+            password.encode("utf-8"), 
+            settings.SECRET_KEY.encode("utf-8"), 
+            100000
+        )
+        hashed_password = binascii.hexlify(dk).decode("utf-8")
+        return self.password == f"pbkdf2_sha256$100000${hashed_password}"
     
     def __repr__(self) -> str:
         return f"<User id={self.id} active={self.active}>"
